bbfbe90ba2f044492860ec85461d03a90b101369
Omar Zaghlool <62655598+spectre-xenon@users.noreply.github.com>
2024-10-07 16:04:16 +0000

    201 current  2024-10-07 16:04:07  24.11.20240926.1925c60   6.6.52                          *

--- modules/home/neovim/default.nix
+++ modules/home/neovim/default.nix
@@ -5,41 +5,41 @@
     vimAlias = true;
     vimdiffAlias = true;
 
-    # extraLuaConfig = ''
-    #   ${builtins.readFile ./config/init.lua}
-    # '';
+    extraLuaConfig = ''
+      ${builtins.readFile ./config/init.lua}
+    '';
   };
 
-  # xdg.configFile."nvim/lua" = {
-  #   source = ./config/lua;
-  #   recursive = true;
-  # };
+  xdg.configFile."nvim/lua" = {
+    source = ./config/lua;
+    recursive = true;
+  };
 
-  # # set Colorscheme using stylix colors
-  # stylix.targets.neovim.enable = false;
-  # home.file.".config/nvim/lua/plugins/base16.lua".text = with config.lib.stylix.colors.withHashtag; ''
-  #   return {
-  #     'RRethy/base16-nvim',
-  #     config = function()
-  #       require('base16-colorscheme').setup {
-  #         base00 = '${base00}',
-  #         base01 = '${base01}',
-  #         base02 = '${base02}',
-  #         base03 = '${base03}',
-  #         base04 = '${base04}',
-  #         base05 = '${base05}',
-  #         base06 = '${base06}',
-  #         base07 = '${base07}',
-  #         base08 = '${base08}',
-  #         base09 = '${base09}',
-  #         base0A = '${base0A}',
-  #         base0B = '${base0B}',
-  #         base0C = '${base0C}',
-  #         base0D = '${base0D}',
-  #         base0E = '${base0E}',
-  #         base0F = '${base0F}',
-  #       }
-  #     end,
-  #   }
-  # '';
+  # set Colorscheme using stylix colors
+  stylix.targets.neovim.enable = false;
+  home.file.".config/nvim/lua/plugins/base16.lua".text = with config.lib.stylix.colors.withHashtag; ''
+    return {
+      'RRethy/base16-nvim',
+      config = function()
+        require('base16-colorscheme').setup {
+          base00 = '${base00}',
+          base01 = '${base01}',
+          base02 = '${base02}',
+          base03 = '${base03}',
+          base04 = '${base04}',
+          base05 = '${base05}',
+          base06 = '${base06}',
+          base07 = '${base07}',
+          base08 = '${base08}',
+          base09 = '${base09}',
+          base0A = '${base0A}',
+          base0B = '${base0B}',
+          base0C = '${base0C}',
+          base0D = '${base0D}',
+          base0E = '${base0E}',
+          base0F = '${base0F}',
+        }
+      end,
+    }
+  '';
 }
