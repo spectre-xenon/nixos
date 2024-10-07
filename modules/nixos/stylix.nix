cf896507540ed339097338026374b0f38409b59a
Omar Zaghlool <62655598+spectre-xenon@users.noreply.github.com>
2024-10-07 16:15:30 +0000

    204 current  2024-10-07 16:15:20  24.11.20240926.1925c60   6.6.52                          *

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -3,7 +3,7 @@
     enable = true;
 
     # ColorScheme
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     # Wallpaper
     image = ../../wallpaper.jpg;
     # Cursor
