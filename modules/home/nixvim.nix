821c112296940fc82fe9cbc04babb56d2368794f
Omar Zaghlool <62655598+spectre-xenon@users.noreply.github.com>
2024-10-10 16:37:12 +0000

    221 current  2024-10-10 16:36:59  24.11.20240926.1925c60   6.6.52                          *

--- modules/home/nixvim.nix
+++ modules/home/nixvim.nix
@@ -76,12 +76,19 @@
       cmp = {
         enable = true;
         autoEnableSources = true;
-        settings.sources = [
-          {name = "nvim_lsp";}
-          {name = "path";}
-          {name = "buffer";}
-          {name = "luasnip";}
-        ];
+        settings = {
+          sources = [
+            {name = "nvim_lsp";}
+            {name = "path";}
+            {name = "buffer";}
+            {name = "luasnip";}
+          ];
+          mapping = {
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          };
+        };
       };
     };
   };
