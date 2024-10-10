d609cc25e6554a24d4e67143900654f51effec7d
Omar Zaghlool <62655598+spectre-xenon@users.noreply.github.com>
2024-10-10 15:41:32 +0000

    210 current  2024-10-10 15:41:20  24.11.20240926.1925c60   6.6.52                          *

--- modules/home/nixvim.nix
+++ modules/home/nixvim.nix
@@ -1,5 +1,16 @@
 {
   programs.nixvim = {
     enable = true;
+    plugins = {
+      lualine.enable = true;
+      treesitter = {
+        enable = true;
+        settings = {
+          indent = {enable = true;};
+          highlight = {enable = true;};
+          auto_install = true;
+        };
+      };
+    };
   };
 }
