75eaaf9bc0f86adca3b40d90c412cbf8beb7f275
Omar Zaghlool <62655598+spectre-xenon@users.noreply.github.com>
2024-10-10 15:54:49 +0000

    213 current  2024-10-10 15:54:38  24.11.20240926.1925c60   6.6.52                          *

--- modules/home/nixvim.nix
+++ modules/home/nixvim.nix
@@ -2,13 +2,21 @@
   programs.nixvim = {
     enable = true;
     plugins = {
+      # Lualine
       lualine.enable = true;
+      # Treesitter
       treesitter = {
         enable = true;
         settings = {
           indent.enable = true;
           highlight.enable = true;
           auto_install = true;
+        };
+      };
+      # LSP
+      lsp = {
+        servers = {
+          nil_ls.enable = true;
         };
       };
     };
