34e58021c7baebb7a10be90bfb3b497f3b3c0d65
Omar Zaghlool <62655598+spectre-xenon@users.noreply.github.com>
2024-10-11 13:03:54 +0000

    239 current  2024-10-11 13:03:34  24.11.20240926.1925c60   6.6.52                          *

--- users/xen/home.nix
+++ users/xen/home.nix
@@ -11,9 +11,9 @@
     ../../modules/home/fish.nix
     ../../modules/home/starship
     ../../modules/home/tofi.nix
-    ../../modules/home/ags
-    inputs.nixvim.homeManagerModules.nixvim
-    ../../modules/home/nixvim.nix
+    # inputs.nixvim.homeManagerModules.nixvim
+    # ../../modules/home/nixvim.nix
+    ../../modules/home/neovim
     ../../modules/home/gtk.nix
     ../../modules/home/packages.nix
   ];
