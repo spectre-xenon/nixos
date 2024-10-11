a0612ca306aff6d3011324e6f5a5c2428f55c2fa
Omar Zaghlool <62655598+spectre-xenon@users.noreply.github.com>
2024-10-11 13:43:17 +0000

    243 current  2024-10-11 13:43:04  24.11.20240926.1925c60   6.6.52                          *

--- modules/home/neovim/config/lua/plugins/treesitter.lua
+++ modules/home/neovim/config/lua/plugins/treesitter.lua
@@ -3,23 +3,24 @@ return {
   'nvim-treesitter/nvim-treesitter',
   version = false, -- last release is way too old and doesn't work on Windows
   build = ':TSUpdate',
-  event = { 'VeryLazy' },
-  lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-  init = function(plugin)
-    -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-    -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-    -- no longer trigger the **nvim-treesitter** module to be loaded in time.
-    -- Luckily, the only things that those plugins need are the custom queries, which we make available
-    -- during startup.
-    require('lazy.core.loader').add_to_rtp(plugin)
-    require 'nvim-treesitter.query_predicates'
-  end,
-  cmd = { 'TSUpdateSync', 'TSUpdate', 'TSInstall' },
-  ---@type TSConfig
-  ---@diagnostic disable-next-line: missing-fields
-  opts = {
-    auto_install = true,
-    highlight = { enable = true },
-    indent = { enable = true },
-  },
+  config = function()
+    require'nvim-treesitter.configs'.setup {
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+      auto_install = true,
+      highlight = {
+        enable = true,
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true,
+      },
+    }
+  end
 }
