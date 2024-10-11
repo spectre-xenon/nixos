a6f6eb5fd0e04b45d85f9d5aaa124d0ac0f943b8
Omar Zaghlool <62655598+spectre-xenon@users.noreply.github.com>
2024-10-11 20:17:19 +0000

    292 current  2024-10-11 20:17:04  24.11.20240926.1925c60   6.6.52                          *

--- modules/home/neovim/config/lua/plugins/conform.lua
+++ modules/home/neovim/config/lua/plugins/conform.lua
@@ -6,16 +6,8 @@ return {
   keys = {
     {
       "<leader>f",
-      function(args)
-        local range = nil
-        if args.count ~= -1 then
-          local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-          range = {
-            start = { args.line1, 0 },
-            ["end"] = { args.line2, end_line:len() },
-          }
-        end
-        require("conform").format({ async = true, lsp_format = "fallback", range = range })
+      function()
+        require("conform").format({ async = true, lsp_format = "fallback" })
       end,
       mode = { "n", "v" },
       desc = "Format Injected Langs",
