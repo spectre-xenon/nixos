44b388e45732db77eb4d619f120c1dc7d3a97818
Omar Zaghlool <62655598+spectre-xenon@users.noreply.github.com>
2024-10-11 19:35:02 +0000

    285 current  2024-10-11 19:34:46  24.11.20240926.1925c60   6.6.52                          *

--- modules/home/neovim/config/lua/plugins/todocomments.lua
+++ modules/home/neovim/config/lua/plugins/todocomments.lua
@@ -1,7 +1,7 @@
 return {
   "folke/todo-comments.nvim",
   cmd = { "TodoTrouble", "TodoTelescope" },
-  event = "LazyFile",
+  event ={ "BufReadPost", "BufNewFile", "BufWritePre" },
   opts = {},
   -- stylua: ignore
   keys = {
