3a9f7ed532f0bc68a8413e4ac4fcd6f87dd61130
Omar Zaghlool <62655598+spectre-xenon@users.noreply.github.com>
2024-10-11 15:27:07 +0000

    253 current  2024-10-11 15:26:49  24.11.20240926.1925c60   6.6.52                          *

--- modules/home/neovim/config/lua/plugins/telescope.lua
+++ modules/home/neovim/config/lua/plugins/telescope.lua
@@ -12,7 +12,17 @@ return {
   },
   config = function()
     -- Setup
-    require('telescope').setup()
+    require('telescope').setup(
+      {
+        -- Close on first ESC
+        defaults = {
+          mappings = {
+            i = {
+              ["<esc>"] = actions.close,
+            },
+          },
+        },
+      })
     -- To get fzf loaded and working with telescope, you need to call
     -- load_extension, somewhere after setup function:
     require('telescope').load_extension('fzf') 
