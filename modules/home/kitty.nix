a2b8be56246cb13e9298ed51bf863f2b441a339c
Omar Zaghlool <62655598+spectre-xenon@users.noreply.github.com>
2024-10-11 12:36:42 +0000

    237 current  2024-10-11 12:36:26  24.11.20240926.1925c60   6.6.52                          *

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -2,6 +2,7 @@
   programs.kitty = {
     enable = true;
     settings = {
+      disable_ligatures = "always";
       window_padding_width = 0;
       hide_window_decorations = "yes";
       confirm_os_window_close = 0;
--- modules/home/neovim/config/lua/options.lua
+++ modules/home/neovim/config/lua/options.lua
@@ -21,9 +21,29 @@ vim.opt.showmode = false
 
 -- Enable break indent
 vim.opt.breakindent = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- No wrap
+vim.opt.wrap = false
 
 -- Save undo history
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/.undodir"
 vim.opt.undofile = true
+
+-- Disable highlight after search but enable incermental serach
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Terminal coooloors
+vim.opt.termguicolors = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
@@ -39,21 +59,11 @@ vim.opt.updatetime = 250
 -- Displays which-key popup sooner
 vim.opt.timeoutlen = 300
 
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
-
--- Show which line your cursor is on
-vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
