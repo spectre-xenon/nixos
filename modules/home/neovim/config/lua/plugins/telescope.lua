d4d17c0f05e4d27a21688238bc290ee66c83a0dc
Omar Zaghlool <62655598+spectre-xenon@users.noreply.github.com>
2024-10-11 15:39:20 +0000

    255 current  2024-10-11 15:39:06  24.11.20240926.1925c60   6.6.52                          *

--- modules/home/neovim/config/lua/plugins/telescope.lua
+++ modules/home/neovim/config/lua/plugins/telescope.lua
@@ -4,27 +4,30 @@ return {
     'nvim-lua/plenary.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
+
+  opts = {
+    defaults = {
+      mappings = {
+        i = {
+          -- Close on first ESC
+          ["<esc>"] = require"telescope.actions".close,
+        },
+      },
+    },
+  },
+
+  config = function(_ , opts)
+    -- Setup
+    require('telescope').setup(opts)
+    -- To get fzf loaded and working with telescope, you need to call
+    -- load_extension, somewhere after setup function:
+    require('telescope').load_extension('fzf') 
+  end,
+
   keys = {
     { "<leader><space>", function() require'telescope.builtin'.find_files() end, },
     { '<C-p>', function() require'telescope.builtin'.git_files() end,  },
     { '<leader>/', function() require'telescope.builtin'.live_grep() end,  },
     { '<leader>fb', function() require'telescope.builtin'.buffers() end,  },
   },
-  config = function()
-    -- Setup
-    require('telescope').setup(
-      {
-        -- Close on first ESC
-        defaults = {
-          mappings = {
-            i = {
-              ["<esc>"] = require"telescope.actions".close,
-            },
-          },
-        },
-      })
-    -- To get fzf loaded and working with telescope, you need to call
-    -- load_extension, somewhere after setup function:
-    require('telescope').load_extension('fzf') 
-  end,
 }
