32d6c54076e111f7b77598c082fd47a48bc11ff3
Omar Zaghlool <62655598+spectre-xenon@users.noreply.github.com>
2024-10-11 13:28:17 +0000

    241 current  2024-10-11 13:28:00  24.11.20240926.1925c60   6.6.52                          *

--- modules/home/neovim/config/lua/plugins/telescope.lua
+++ modules/home/neovim/config/lua/plugins/telescope.lua
@@ -4,12 +4,16 @@ return {
     'nvim-lua/plenary.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
+  opts = {},
   config = function()
+    -- To get fzf loaded and working with telescope, you need to call
+    -- load_extension, somewhere after setup function:
+    require('telescope').load_extension('fzf') 
+    -- Keymaps
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader><space>', builtin.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope git files' })
     vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = 'Telescope live grep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
   end,
-
 }
