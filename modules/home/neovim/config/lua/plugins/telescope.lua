9224ddb3c69f130133af3b6e0a12887a7f92399d
Omar Zaghlool <62655598+spectre-xenon@users.noreply.github.com>
2024-10-11 15:00:45 +0000

    250 current  2024-10-11 15:00:25  24.11.20240926.1925c60   6.6.52                          *

--- modules/home/neovim/config/lua/plugins/telescope.lua
+++ modules/home/neovim/config/lua/plugins/telescope.lua
@@ -5,10 +5,10 @@ return {
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
   keys = {
-    { "<leader><space>", function() require'telescope.builtin'.find_files end, desc = 'Telescope find files' },
-    { '<C-p>', function() require'telescope.builtin'.git_files end,  desc = 'Telescope git files' },
-    { '<leader>/', function() require'telescope.builtin'.live_grep end,  desc = 'Telescope live grep' },
-    { '<leader>fb', function() require'telescope.builtin'.buffers end,  desc = 'Telescope buffers' },
+    { "<leader><space>", function() require'telescope.builtin'.find_files() end, },
+    { '<C-p>', function() require'telescope.builtin'.git_files() end,  },
+    { '<leader>/', function() require'telescope.builtin'.live_grep() end,  },
+    { '<leader>fb', function() require'telescope.builtin'.buffers() end,  },
   },
   config = function()
     -- Setup
@@ -16,7 +16,5 @@ return {
     -- To get fzf loaded and working with telescope, you need to call
     -- load_extension, somewhere after setup function:
     require('telescope').load_extension('fzf') 
-    -- Keymaps
-    local builtin = require('telescope.builtin')
   end,
 }
