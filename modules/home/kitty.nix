6457b91993a123b9aff2c526d44111208cedda10
Omar Zaghlool <62655598+spectre-xenon@users.noreply.github.com>
2024-10-11 12:06:34 +0000

    233 current  2024-10-11 12:06:14  24.11.20240926.1925c60   6.6.52                          *

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -2,7 +2,7 @@
   programs.kitty = {
     enable = true;
     settings = {
-      window_padding_width = 0;
+      window_padding_width = 2;
       hide_window_decorations = "yes";
       confirm_os_window_close = 0;
     };
