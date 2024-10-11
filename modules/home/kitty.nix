4c72fa56036d7fe1700d8e1337b21a5a16539c80
Omar Zaghlool <62655598+spectre-xenon@users.noreply.github.com>
2024-10-11 14:43:42 +0000

    245 current  2024-10-11 14:43:26  24.11.20240926.1925c60   6.6.52                          *

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -3,6 +3,7 @@
     enable = true;
     settings = {
       disable_ligatures = "always";
+      placement_strategy = "center";
       window_padding_width = 0;
       hide_window_decorations = "yes";
       confirm_os_window_close = 0;
